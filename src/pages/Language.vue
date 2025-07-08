ef1098371cecf8d5de9e3e04db7038a8e9efa52d
TTS <theintheinsoe1218@gmail.com>
2025-07-08 13:39:52 +0630

    update language

--- src/pages/Language.vue
+++ src/pages/Language.vue
@@ -10,7 +10,6 @@
               <th class="text-center white--text bg-primary">Amount</th>
               <th class="text-center white--text bg-primary">Exam Link</th>
               <th class="text-center white--text bg-primary">Exam Fee</th>
-
               <th class="text-center white--text bg-primary">Action</th>
             </tr>
           </thead>
@@ -27,22 +26,23 @@
               }"
             >
               <td class="text-center">{{ index + 1 }}</td>
-              <td class="text-center">{{ item.name }}</td>
-              
-              <td class="text-center">{{ item.amount }}</td>
+              <td class="text-center">{{ item?.name }}</td>
 
-              <td class="text-start">{{ item.examLink }}</td>
-              <td class="text-start">{{ item.examFee }}</td>
+              <td class="text-start">{{ item?.amount }}</td>
 
-              
+              <td class="text-start">{{ item?.examLink }}</td>
+              <td class="text-start">{{ item?.examFee }}</td>
 
               <td class="text-center">
-                
                 <v-btn class="ml-1" small icon color="green" density="compact">
-                  <v-icon size="small" @click="editUser(item)">mdi-pencil</v-icon></v-btn
+                  <v-icon size="small" @click="clickEdit(item)"
+                    >mdi-pencil</v-icon
+                  ></v-btn
                 >
                 <v-btn class="ml-1" small icon color="red" density="compact">
-                  <v-icon size="small" @click="deleteUserMethod(item)">mdi-delete</v-icon></v-btn
+                  <v-icon size="small" @click="clickDelete(item)"
+                    >mdi-delete</v-icon
+                  ></v-btn
                 >
               </td>
             </tr>
@@ -56,7 +56,6 @@
         <v-col>
           <v-card variant="outlined" class="mr-2 pl-2 pr-2">
             <v-row no-gutters class="pa-2">
-              
               <v-col cols="12" md="12">
                 <v-text-field
                   label="Name"
@@ -68,7 +67,7 @@
               </v-col>
               <v-col cols="12" md="12">
                 <v-text-field
-                type="number"
+                  type="number"
                   label="Amount"
                   v-model="language.amount"
                   :rules="[(v) => !!v || 'required']"
@@ -81,24 +80,27 @@
                   label="Exam Link"
                   v-model="language.examLink"
                   :rules="[(v) => !!v || 'required']"
-                   density="compact"
+                  density="compact"
                   variant="outlined"
                 ></v-text-field>
               </v-col>
               <v-col cols="12" md="12">
                 <v-text-field
-                type="number"
+                  type="number"
                   label="Exam Fee"
                   v-model="language.examFee"
                   density="compact"
                   variant="outlined"
                 ></v-text-field>
               </v-col>
-              
+
               <v-col cols="12" md="12" class="text-right mb-2">
-                <v-btn color="primary " @click="saveUser()">{{
-                  saveOrupdate
-                }}</v-btn>
+                <v-btn
+                  color="primary "
+                  :disabled="!isFormValid"
+                  @click="saveLanguage()"
+                  >{{ saveOrupdate }}</v-btn
+                >
               </v-col>
             </v-row>
           </v-card>
@@ -108,26 +110,32 @@
     <v-col>
       <v-dialog v-model="dialogDelete" width="500">
         <v-card>
-          <v-card-title class="text-h5 white--text bg-red"> Delete </v-card-title>
+          <v-card-title class="text-h5 white--text bg-red">
+            Delete
+          </v-card-title>
 
           <v-card-text class="text-h6">
             Are you sure to delete({{ selectedOne.name }})?
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn class="black" text @click="dialogDelete = false"> CANCEL </v-btn>
-            <v-btn dark class="bg-red" text @click="clickDeleteDialog()"> DELETE </v-btn>
+            <v-btn class="black" text @click="dialogDelete = false">
+              CANCEL
+            </v-btn>
+            <v-btn dark class="bg-red" text @click="clickDeleteDialog()">
+              DELETE
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
     </v-col>
     <v-col>
-          <v-bottom-sheet v-model="userPhotoDialog" fullscreen scrollable>
+      <v-bottom-sheet v-model="userPhotoDialog" fullscreen scrollable>
         <v-sheet class="information-window-v-sheet">
           <PhotoUser
-          @closeDialog="userPhotoDialog=false"
-          :user="selectedOne"
-          @loadUserList="loadUserList"
+            @closeDialog="userPhotoDialog = false"
+            :user="selectedOne"
+            @loadUserList="loadUserList"
           />
         </v-sheet>
       </v-bottom-sheet>
@@ -135,53 +143,49 @@
   </v-row>
 </template>
 <script>
-import { format } from "date-fns";
 import languageService from "../service/LanguageService.js";
 export default {
   data: () => ({
-    selectedOne:{},
-    language:{},
-    saveOrupdate:"SAVE",
-    languageList:[],
+    selectedOne: {},
+    language: {},
+    saveOrupdate: "SAVE",
+    languageList: [],
+    dialogDelete: false,
   }),
-  props: {  },
+  props: {},
   mounted: function () {
     this.languageListMethod();
   },
   methods: {
-    
-    saveUser: function () {
+    saveLanguage: function () {
       if (this.saveOrupdate == "SAVE") {
         languageService
           .addLanguageList(this.language)
           .then((response) => {
+            this.$swal({
+              icon: "success",
+              title: "Your work has been saved",
+              showConfirmButton: false,
+              timer: 1000,
+            });
             this.language = {};
-            
-            // this.user = {};
-            // this.userListMethod();
-            // this.$swal({
-            //   icon: "success",
-            //   title: "Your work has been saved",
-            //   showConfirmButton: false,
-            //   timer: 1000,
-            // });
+            this.languageListMethod();
           })
           .catch((error) => {
             this.$swal("Fail!", error.response.data.message, "error");
           });
       } else {
-        userService
-          .updateUser(this.user)
+        languageService
+          .updateLanguage(this.language)
           .then((response) => {
-            this.saveOrupdate="SAVE";
-            this.user = {};
-            this.userListMethod();
             this.$swal({
               icon: "success",
               title: "Your work has been updated",
               showConfirmButton: false,
               timer: 1000,
             });
+            this.language = {};
+            this.languageListMethod();
           })
           .catch((error) => {
             this.$swal("Fail!", error.response.data.message, "error");
@@ -192,20 +196,51 @@ export default {
       languageService
         .getLanguageList()
         .then((response) => {
-          // console.log(response);
-          
           this.languageList.splice(0);
           this.languageList.push(...response);
         })
         .catch((error) => {
-          // this.$swal("Fail!", error.response.data.message, "error");
+          this.$swal("Fail!", error.response.data.message, "error");
+        });
+    },
+    clickEdit(item) {
+      this.language = { ...item };
+      this.saveOrupdate = "UPDATE";
+    },
+    clickDelete(item) {
+      this.selectedOne = { ...item };
+      this.dialogDelete = true;
+    },
+    clickDeleteDialog() {
+      languageService
+        .deleteLanguage(this.selectedOne)
+        .then((response) => {
+          this.dialogDelete = false;
+          this.$swal({
+            icon: "success",
+            title: "Your work has been deleted",
+            showConfirmButton: false,
+            timer: 1000,
+          });
+          this.languageListMethod();
+        })
+        .catch((error) => {
+          this.$swal("Fail!", error.response.data.message, "error");
         });
     },
   },
-  watch: {
-    
-  },
+  watch: {},
   components: {},
+  computed: {
+    isFormValid() {
+      return (
+        this.language.name &&
+        this.language.amount &&
+        this.language.examLink &&
+        this.language.examFee
+      );
+    },
+  },
 };
 </script>
 <style scoped>
